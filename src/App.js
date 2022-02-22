378559e84279548979d5ab7fcfac3bd3d7ea6912
tranggot config --global user.email <trangnguyenphamngoc@gmail.com>
2022-02-22 23:18:03 +0800

    broken our homepage into smaller components, finished styling our menu-item

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import HomePage from "./homepage.components";
+import HomePage from "./pages/homepage/hompage.component";
 
 function App() {
   return (
