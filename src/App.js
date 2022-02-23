9e5ebaf3ae6242588e1e3732a3c7eeedd1623ec1
tranggot config --global user.email <trangnguyenphamngoc@gmail.com>
2022-02-23 11:17:49 +0800

    adding header component and links to Home, shop page and contact

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import { Route } from "react-router-dom/cjs/react-router-dom.min";
 import HomePage from "./pages/homepage/hompage.component";
 import ShopPage from "./pages/shop/shop.component";
 import { Switch } from "react-router-dom";
+import Header from "./components/header/header.component";
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path={"/"} component={HomePage} />
         <Route path="/shop" component={ShopPage} />
